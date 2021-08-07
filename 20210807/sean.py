a96d1de37460201b89b2d1573c8f7682b23308a1
rubikscraft54 <rubikscraft54@gmail.com>
2021-08-07 11:52:22 +0900

    Update sean.py

--- 20210807/sean.py
+++ 20210807/sean.py
@@ -3,3 +3,19 @@ def solution(price, money, count):
   if total > money:
     return total - money
   return 0
+
+def solution(record):
+    nameDict = {}
+    answer = []
+    for i in range(len(record)):
+        s = record[i].split()
+        if s[0] == 'Change':
+            nameDict[s[1]] = s[2]
+        elif s[0] == 'Enter':
+            nameDict[s[1]] = s[2]
+            answer.append(f'{s[1]} 님이 들어왔습니다.')
+        elif s[0] == 'Leave':
+            answer.append(f'{s[1]} 님이 나갔습니다.')
+    for i in range(len(answer)):
+        s = answer[i].split()
+        answer[i] = (answer[i].replace(s[0], nameDict[s[0]])).replace(' ', "", 1)
