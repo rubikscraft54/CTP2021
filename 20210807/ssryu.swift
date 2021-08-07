92b9fbf11490310238322d08e72d686079b1b3c1
sky0426 <tmdtjrdl@gmail.com>
2021-08-07 11:47:20 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42888 문제풀이

--- 20210807/ssryu.swift
+++ 20210807/ssryu.swift
@@ -19,3 +19,56 @@ func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
 }
 
 
+
+///https://programmers.co.kr/learn/courses/30/lessons/42888
+func solution(_ record:[String]) -> [String] {
+    
+    let chatRoom = ChatRoom()
+    for rec in record {
+        chatRoom.command(rec)
+    }
+    return chatRoom.makeStringArray()
+}
+
+class ChatRoom {
+    var nickNameDic:[String:String] = [:]
+    var messages: [Message] = []
+    
+    func command(_ command: String) {
+        let strs = command.components(separatedBy: " ")
+        if strs[0] == "Enter" {
+            nickNameDic[strs[1]] = strs[2]
+            messages.append(Message(strs[1], Status.enter))
+        } else if strs[0] == "Change" {
+            nickNameDic[strs[1]] = strs[2]
+        } else {
+            messages.append(Message(strs[1], Status.leave))
+        }
+    }
+    
+    func makeStringArray() -> [String] {
+        var result: [String] = []
+        
+        for message in messages {
+            result.append(nickNameDic[message.uid]! + message.status)
+        }
+        return result
+    }
+}
+
+
+
+struct Message {
+    let uid: String
+    let status: String
+    
+    init(_ uid: String, _ status: Status) {
+        self.uid = uid
+        self.status = status.rawValue
+    }
+}
+
+enum Status: String {
+    case enter = "님이 들어왔습니다."
+    case leave = "님이 나갔습니다."
+}
